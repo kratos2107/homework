c2bfad49feca89b92031b9e251d2ee9179c32ec9
kratos2107 <yubaneishvili21@gmail.com>
2022-09-21 00:39:01 +0400

    second commit

---
+++ lesson1.py
@@ -0,0 +1,8 @@
+name="I am luka"
+surname="kubaneishvili"
+age=22
+age1="years old"
+city="from tbilisi"
+occupation="i have been studying in Tbilisi state medical university for 4 years now and still dont know why"
+regret="I just wanted to be happy and play dota 2 "
+print(name+" "+surname+","+str(age)+" "+age1+","+city+","+occupation+","+regret+".")
